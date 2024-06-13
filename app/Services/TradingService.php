8c4803c034d121ac93c324b16b376818d4c6ed91
CyrilleVerleye <160618056+CyrilleVerleye@users.noreply.github.com>
2024-06-13 09:24:15 +0200

    light push

--- app/Services/TradingService.php
+++ app/Services/TradingService.php
@@ -9,21 +9,33 @@ class TradingService
     public function getHistoricalbars()
     {
         try {
-            $response = Http::withHeaders([
-                'APCA-API-KEY-ID' => config('services.alpaca.key'),
-                'APCA-API-SECRET-KEY' => config('services.alpaca.secret'),
-                'Accept' => 'application/json',
-            ])->get('https://data.alpaca.markets/v2/stocks/bars', [
-                'symbols' => 'AAPL',
-                'timeframe' => '1Day',
-                'start' => '2024-01-01T00:00:00Z',
-                'limit' => 1000,
-                'adjustment' => 'raw',
-                'feed' => 'iex',
-                'sort' => 'asc'
-            ]);
+            $data = [];
+            $nextPageUrl = null;
 
-            return $response->json();
+            do {
+                // Fetch data with the pagination URL
+                $response = Http::withHeaders([
+                    'APCA-API-KEY-ID' => config('services.alpaca.key'),
+                    'APCA-API-SECRET-KEY' => config('services.alpaca.secret'),
+                    'Accept' => 'application/json',
+                ])->get($nextPageUrl ?: 'https://data.alpaca.markets/v2/stocks/bars', [
+                    'symbols' => 'AAPL',
+                    'timeframe' => '1Min',
+                    'start' => '2024-01-01T00:00:00Z',
+                    'limit' => 1000,
+                    'adjustment' => 'raw',
+                    'feed' => 'iex',
+                    'sort' => 'asc',
+                ]);
+
+                $responseData = $response->json();
+                $data = array_merge($data, $responseData);
+
+                // Update the pagination URL for the next page
+                $nextPageUrl = isset($responseData['next_page_url']) ? $responseData['next_page_url'] : null;
+            } while ($nextPageUrl);
+
+            return $data;
         } catch (\Exception $e) {
             return response()->json([
                 'error' => $e->getMessage()
