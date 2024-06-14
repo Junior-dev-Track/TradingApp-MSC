66ea4c676d4f80e258cd39f1b54a395a9eee0e73
CyrilleVerleye <160618056+CyrilleVerleye@users.noreply.github.com>
2024-06-14 10:18:52 +0200

    main json add

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,13 +1,13 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
-import { useState, useEffect } from 'react';
-import { PageProps } from '@/types';
-import PortfolioSummary from '@/Pages/Auth/PortfolioSummary';  // Import the correct module
-import PortfolioDistribution from '@/Pages/Auth/PortfolioDistribution';  // Import the correct module
-import HistoricalBars from './Trading/HistoricalBars';
-import { Doughnut } from 'react-chartjs-2';
-import Chart from 'chart.js/auto';
-import Icons from '@/Pages/Auth/Icons'; // Comment out the import statement for Icons
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
+import { useState, useEffect } from "react";
+import { PageProps } from "@/types";
+import PortfolioSummary from "@/Pages/Auth/PortfolioSummary"; // Import the correct module
+import PortfolioDistribution from "@/Pages/Auth/PortfolioDistribution"; // Import the correct module
+import HistoricalBars from "./Trading/HistoricalBars";
+import { Doughnut } from "react-chartjs-2";
+import Chart from "chart.js/auto";
+import Icons from "@/Pages/Auth/Icons"; // Comment out the import statement for Icons
 
 export default function Dashboard({ auth }: PageProps) {
     return (
@@ -26,8 +26,11 @@ export default function Dashboard({ auth }: PageProps) {
                         <PortfolioSummary />
                     </div>
                     <div>
-                    <Icons/>
+                        <Icons />
                     </div>
+                </section>
+                <section>
+                    <HistoricalBars />
                 </section>
             </AuthenticatedLayout>
         </>
