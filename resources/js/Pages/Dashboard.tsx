2ac6e08e1c62ab6c983fd512c5ea91fc58be21c4
Bekono4526 <bekonorosy0@gmail.com>
2024-06-14 09:36:09 +0200

    merge

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -4,9 +4,9 @@ import { useState, useEffect } from 'react';
 import { PageProps } from '@/types';
 import PortfolioSummary from '@/Pages/Auth/PortfolioSummary';  // Import the correct module
 import PortfolioDistribution from '@/Pages/Auth/PortfolioDistribution';  // Import the correct module
+import HistoricalBars from './Trading/HistoricalBars';
 /**import TradingWallet from '@/Pages/Auth/TradingWallet'; // Import the TradingWallet component */
-import { Doughnut } from 'react-chartjs-2';
-import Chart from 'chart.js/auto';
+
 
 
 
@@ -31,26 +31,28 @@ export default function Dashboard({ auth }: PageProps) {
 
                 <section>
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                        <div className="bg-gray-100 p-4 rounded-lg shadow">
+                        <div className="">
                         {/**<TradingWallet /> */}
                         </div>
-                        <div className="bg-gray-100 p-4 rounded-lg shadow">
+                        <div className="">
+                            {/* Placeholder for another component */}
+                            <HistoricalBars />
+                        </div>
+                        <div className="">
                             {/* Placeholder for another component */}
                         </div>
-                        <div className="bg-gray-100 p-4 rounded-lg shadow">
+                        <div className="">
                             {/* Placeholder for another component */}
                         </div>
-                        <div className="bg-gray-100 p-4 rounded-lg shadow">
+                        <div className="">
                             {/* Placeholder for another component */}
                         </div>
-                        <div className="bg-gray-100 p-4 rounded-lg shadow">
-                            {/* Placeholder for another component */}
-                        </div>
-                        <div className="bg-gray-100 p-4 rounded-lg shadow">
+                        <div className="">
                             {/* Placeholder for another component */}
                         </div>
                     </div>
                 </section>
+
             </AuthenticatedLayout>
         </>
     );
