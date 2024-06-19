d47c53c8dd14af1cea746f639785a09aeb6bb10a
Marilou <marilou.ncks@gmail.com>
2024-06-19 14:42:58 +0200

    Revert "align"
    This reverts commit c7540eb3fef260640db692b12a932cc8a9b19763.

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -3,7 +3,7 @@ import { Head } from '@inertiajs/react';
 import { PageProps } from '@/types';
 import PortfolioSummary from './Auth/PortfolioSummary';
 import HistoricalBars from './Trading/HistoricalBars';
-import TradingWallet from './Auth/TradingWallet';
+// import TradingWallet from './Auth/TradingWallet';
 import Icons from '@/Pages/Auth/Icons';
 import AlertsManager from './Auth/AlertsManager';
 import React, { useState } from 'react';
@@ -22,6 +22,8 @@ export default function Dashboard({ auth }: PageProps) {
 
                     <div className="w-5/7 py-1 bg-gray-800 min-h-screen p-1 w-9/12 ml-24">  {/* Adjusted global container size */}
                         <div className="grid grid-cols-3 grid-rows-2 gap-4 h-1/3">
+                    <div className="w-5/7 py-1 bg-gray-800 min-h-screen p-1 w-9/12 ml-24">  {/* Adjusted global container size */}
+                        <div className="grid grid-cols-3 grid-rows-2 gap-4 h-1/3">
                             {/* Main content widgets */}
                             {/* First Row */}
                             <div className="bg-gray-700 p-4 rounded-lg shadow col-span-1">
@@ -35,7 +37,7 @@ export default function Dashboard({ auth }: PageProps) {
                             </div>
 
                             {/* Second Row */}
-                            <div className="col-span-3 bg-gray-700 p-4 rounded-lg shadow  h-3/5">
+                            <div className="col-span-3 bg-gray-700 p-4 rounded-lg shadow ">
                                 <HistoricalBars />
                             </div>
                         </div>
