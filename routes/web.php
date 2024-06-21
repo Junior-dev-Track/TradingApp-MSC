4958ae0062ff0e61ef19aeda3c27b20f68f36753
CyrilleVerleye <160618056+CyrilleVerleye@users.noreply.github.com>
2024-06-21 10:09:59 +0200

    h

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
     Route::get('/notifications', [NotificationsController::class, 'index'])->name('notifications');
 
     Route::get('/historical-bars', [APIFetch::class, 'getHistoricalbars']);
