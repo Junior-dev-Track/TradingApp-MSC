74550c9cd090bcc58cbe5e6587e1bf260ae1b91a
Marilou <marilou.ncks@gmail.com>
2024-06-21 11:38:58 +0200

    f

--- resources/js/Pages/Trading/HistoricalBars.tsx
+++ resources/js/Pages/Trading/HistoricalBars.tsx
@@ -69,6 +69,18 @@ return (
             >
               Add to Favorites
             </button>
+
+                {/* STYLE BOUTON HOVER
+        <div className="mt-4 w-20
+          bg-blue-950 text-blue-500 border border-blue-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md
+          hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
+            <button
+              className=" p-2 rounded mr-2
+              bg-blue-500 shadow-blue-500 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50
+              group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"
+              onClick={() => onAddFavorite(filteredData[0].symbol)}
+            > */}
+            
             <button
               className="bg-green-500 p-2 rounded"
               onClick={() => onAddPurchase(filteredData[0])}
