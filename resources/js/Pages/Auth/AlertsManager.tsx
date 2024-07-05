0b6a9badf05d75b943bdad904d1dfe0c58cf2536
Marilou <marilou.ncks@gmail.com>
2024-07-05 11:48:42 +0200

    notif

--- resources/js/Pages/Auth/AlertsManager.tsx
+++ resources/js/Pages/Auth/AlertsManager.tsx
@@ -71,10 +71,10 @@ const AlertsManager: React.FC<AlertsManagerProps> = ({
     return (
         <div className="relative">
             <Link href={`/notifications`} preserveState>
-                <button className="flex flex-col text-white hover:text-gray-500">
-                    <FaBell className=" flex flex-col h-7 w-7 mb-2 ml-10" />
+            <button className="flex flex-col text-white hover:text-gray-500">
+                    <FaBell className="flex flex-col h-7 w-7 mb-2 ml-10" />
                     {notifications.length > 0 && (
-                        <span className="bg-red-500 text-white text-xs rounded-full h-4 w-4 flex items-center justify-center absolute top-0">
+                        <span className="bg-red-500 text-white text-xs rounded-full h-4 w-4 flex items-center justify-center absolute top-0 right-0 -mt-1 -mr-2">
                             {notifications.length}
                         </span>
                     )}
